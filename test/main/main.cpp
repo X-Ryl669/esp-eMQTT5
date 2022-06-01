a3ad4163592657a30c74ac4834d2ffd1f59e4834
X-Ryl669 <boite.pour.spam@gmail.com>
2022-06-01 17:42:34 +0200

    Rework the test code to build with latest esp-idf version (4.5 at time of writing)

--- test/main/main.cpp
+++ test/main/main.cpp
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-#include <esp_event_loop.h>
+#include <esp_event.h>
 #include <esp_log.h>
 #include <esp_wifi.h>
 #include <nvs_flash.h>
@@ -79,32 +79,18 @@ static void connect() {
 }
 
 
-
-static esp_err_t event_handler(void *ctx, system_event_t *event) {
-  switch (event->event_id) {
-    case SYSTEM_EVENT_STA_START:
-      // connect to ap
-      esp_wifi_connect();
-
-      break;
-
-    case SYSTEM_EVENT_STA_GOT_IP:
-      // start mqtt
-      connect();
-
-      break;
-
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-      // reconnect wifi
-      esp_wifi_connect();
-
-      break;
-
-    default:
-      break;
-  }
-
-  return ESP_OK;
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        esp_wifi_connect();
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI("eMQTT5", "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        connect();
+    }
 }
 
 
@@ -113,10 +99,23 @@ extern "C" void app_main() {
   ESP_ERROR_CHECK(nvs_flash_init());
 
   // initialize tcp/ip adapter
-  tcpip_adapter_init();
+  ESP_ERROR_CHECK(esp_netif_init());
 
   // register event handler
-  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  esp_event_handler_instance_t instance_any_id;
+  esp_event_handler_instance_t instance_got_ip;
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
 
   // initialize wifi
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -132,7 +131,7 @@ extern "C" void app_main() {
   wifi_config_t wifi_config = {};
   memcpy(wifi_config.sta.ssid, WIFI_SSID, strlen(WIFI_SSID));
   memcpy(wifi_config.sta.password, WIFI_PASS, strlen(WIFI_PASS));
-  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
 
   // start wifi
   ESP_ERROR_CHECK(esp_wifi_start());
